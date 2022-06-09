379e32c20c619de25a5ef8c31c10fa4dd1c2220e
P <saranya.c.p@capgemini.com>
2022-06-09 12:23:05 +0530

    updated

---
+++ azureacr/main.tf
@@ -0,0 +1,25 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=1.36.1"
+}
+
+
+# ACR
+resource "azurerm_container_registry" "acr" {
+  name                = "acr${var.prefix}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Premium"
+  admin_enabled       = false
+  georeplications {
+    location                = "East US"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+  georeplications {
+    location                = "westeurope"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+}
