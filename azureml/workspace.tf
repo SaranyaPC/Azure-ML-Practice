93152b88b07a680babfd328db0d729472a7fcf25
P <saranya.c.p@capgemini.com>
2022-06-09 16:51:57 +0530

    updated version:

--- azureml/workspace.tf
+++ azureml/workspace.tf
@@ -16,7 +16,7 @@ resource "azurerm_key_vault" "default" {
 }
 
 resource "azurerm_storage_account" "default" {
-  name                     = "stmlworkspace"
+  name                     = "storgemlworkspace"
   location                 = azurerm_resource_group.default.location
   resource_group_name      = azurerm_resource_group.default.name
   account_tier             = "Standard"
