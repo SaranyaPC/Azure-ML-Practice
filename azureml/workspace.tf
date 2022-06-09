94efd8c29ec1a9f851e4601a1e32f175ac763d25
P <saranya.c.p@capgemini.com>
2022-06-09 17:02:52 +0530

    updated version:

--- azureml/workspace.tf
+++ azureml/workspace.tf
@@ -7,7 +7,7 @@ resource "azurerm_application_insights" "default" {
 }
 
 resource "azurerm_key_vault" "default" {
-  name                     = "keyvaultml"
+  name                     = "keyvaultmlworkspace"
   location                 = azurerm_resource_group.default.location
   resource_group_name      = azurerm_resource_group.default.name
   tenant_id                = data.azurerm_client_config.current.tenant_id
