466e791ecb476a923b77cc06d7a9187721a3d38e
P <saranya.c.p@capgemini.com>
2022-06-10 14:29:45 +0530

    updated code

--- azureacr/main.tf
+++ azureacr/main.tf
@@ -16,19 +16,11 @@ provider "azurerm" {
 
 
 # ACR
+
 resource "azurerm_container_registry" "acr" {
-  name                = "acrml1${var.prefix}"
+  name                = "acrml2${var.prefix}"
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = "Premium"
-  georeplications {
-    location                = var.geolocation
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-  georeplications {
-    location                = var.geolocation1
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
+  admin_enabled       = true
 }
--- azureml/workspace.tf
+++ azureml/workspace.tf
@@ -16,7 +16,7 @@ resource "azurerm_key_vault" "default" {
 }
 
 resource "azurerm_storage_account" "default" {
-  name                     = "storage2${var.prefix}"
+  name                     = "storage3${var.prefix}"
   location                 = var.location
   resource_group_name      = var.resource_group_name
   account_tier             = "Standard"
